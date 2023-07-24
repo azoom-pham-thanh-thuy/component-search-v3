89fe43811d35ab2c6cd3cd6f067f7e8c058fa213
Pham Thanh Thuy <pham.thanh.thuy@azoom.jp>
2023-07-24 03:37:59 +0000

    ➕ Add and configure packages

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,82 @@
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.min.css'
+import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+export default createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: { mdi }
+  },
+  defaults: {
+    VBtn: {
+      color: 'primary',
+    },
+    VCheckbox: {
+      hideDetails: 'auto',
+      color: 'primary',
+      density: 'compact'
+    },
+    VRadio: {
+      hideDetails: 'auto',
+      color: 'primary',
+      density: 'compact'
+    },
+    VRadioGroup: {
+      hideDetails: 'auto',
+      color: 'primary',
+      density: 'compact'
+    },
+    VSelect: {
+      hideDetails: 'auto',
+      color: 'primary',
+      density: 'compact',
+      variant: 'outlined'
+    },
+    VAutocomplete: {
+      hideDetails: 'auto',
+      color: 'primary',
+      density: 'compact',
+      variant: 'outlined'
+    },
+    VTextarea: {
+      hideDetails: 'auto',
+      color: 'primary',
+      variant: 'outlined'
+    },
+    VTextField: {
+      hideDetails: 'auto',
+      color: 'primary',
+      density: 'compact',
+      variant: 'outlined'
+    },
+    VList: {
+      bgColor: '#FFFFFF',
+      color: 'primary'
+    },
+    VChip: {
+      color: 'primary',
+      bgColor: '#FFFFFF'
+    },
+  },
+  theme: {
+    defaultTheme: 'defaultTheme',
+    themes: {
+      defaultTheme: {
+        dark: false,
+        colors: {
+          primary: '#6200ee',
+          'primary-light': '#f3f1fd',
+          white: '#fff',
+          gray: '#dfdfdf',
+          black: '#000'
+        }
+      }
+    }
+  }
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vuetify from 'vite-plugin-vuetify'
+import { fileURLToPath, URL } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vuetify()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@root': fileURLToPath(new URL('.', import.meta.url))
+    }
+  }
 })
